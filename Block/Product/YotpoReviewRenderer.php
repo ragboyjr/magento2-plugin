a95cb1b848720e1facdb51596fd9b32971214982
RJ Garcia <rj@bighead.net>
2018-07-18 01:00:11 -0700

    Moving YotpoReviewRenderer file to proper folder
    Signed-off-by: RJ Garcia <rj@bighead.net>

--- Block/Product/YotpoReviewRenderer.php
+++ Block/Product/YotpoReviewRenderer.php
@@ -6,44 +6,32 @@ class YotpoReviewRenderer extends \Magento\Review\Block\Product\ReviewRenderer
 {
     const SCOPE_STORE   = 'store';
     const YOTPO_BOTTOMLINE_CATEGORY_ENABLED = 'yotpo/settings/category_bottomline_enabled';
-    const MAGENTO_DEFAULT_REVIEWS_ENABLED = 'yotpo/settings/mdr_enabled';
-    protected $_availableTemplates = [
-        self::FULL_VIEW => 'helper/yotposummary.phtml',
-        self::SHORT_VIEW => 'helper/summary_short.phtml',
-    ];
-    
+
     public function getReviewsSummaryHtml(
         \Magento\Catalog\Model\Product $product,
         $templateType = self::DEFAULT_VIEW,
         $displayIfNoReviews = false
     ) {
-        
+
         $enableBottomlineCategoryPage = $this->isBottomlineCategoryEnabled();
-	$enableMagentoDefaultReviews = $this->isMagentoDefaultReviewsEnabled();
+
         if ($enableBottomlineCategoryPage) {
             return $this->showCategoryBottomLine($product);
-        } elseif (!$enableMagentoDefaultReviews) {
-            return parent::getReviewsSummaryHtml($product, $templateType, $displayIfNoReviews);
         } else {
-            return '';
+            return parent::getReviewsSummaryHtml($product, $templateType, $displayIfNoReviews);
         }
-        
-         
+
+
     }
-    
-    public function isMagentoDefaultReviewsEnabled()
-    {        
-        return (bool)$this->_scopeConfig->getValue(self::MAGENTO_DEFAULT_REVIEWS_ENABLED, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-    }
-    
+
     public function isBottomlineCategoryEnabled()
-    {        
+    {
         return (bool)$this->_scopeConfig->getValue(self::YOTPO_BOTTOMLINE_CATEGORY_ENABLED, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
     }
     public function showCategoryBottomLine($product)
-    {        
+    {
         return '<div class="yotpo bottomLine" data-product-id="'.$product->getId().'"
-	data-url="'.$product->getUrl().'">
+    data-url="'.$product->getUrl().'">
         </div>';
     }
 }
